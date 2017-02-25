6064a421375b09bac5a291c362ea4a796ab6659f
smk-141510233 <141510233@smkassalaambandung.sch.id>
2017-02-25 07:58:53 +0700

    BISSMILLAH

---
+++ storage/framework/views/371de1ad5b451b3bfb90184b34c220d154763422.php
@@ -0,0 +1,90 @@
+<?php $__env->startSection('content'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PT.pete</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway';
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <?php if(Route::has('login')): ?>
+                
+            <?php endif; ?>
+
+            <div class="content">
+                <div class="title m-b-md">
+                    PT. Pete
+                </div>
+
+                <div class="content">
+                        <h3>Hidup Adalah Udunan</h3> 
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts/app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ ujikom.sql
@@ -0,0 +1,467 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 24, 2017 at 11:16 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ujikom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `golongans`
+--
+
+CREATE TABLE `golongans` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `kode_golongan` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `nama_golongan` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `besaran_uang` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `golongans`
+--
+
+INSERT INTO `golongans` (`id`, `kode_golongan`, `nama_golongan`, `besaran_uang`, `created_at`, `updated_at`) VALUES
+(1, 'KG001', 'Senior', 5000000, '2017-02-24 11:47:19', '2017-02-24 11:47:19'),
+(2, 'KG0002', 'Medium', 4000000, '2017-02-24 12:01:22', '2017-02-24 12:01:29'),
+(3, 'KG0003', 'Junior', 3500000, '2017-02-24 12:02:26', '2017-02-24 12:02:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jabatans`
+--
+
+CREATE TABLE `jabatans` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `kode_jabatan` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `nama_jabatan` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `besaran_uang` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `jabatans`
+--
+
+INSERT INTO `jabatans` (`id`, `kode_jabatan`, `nama_jabatan`, `besaran_uang`, `created_at`, `updated_at`) VALUES
+(1, 'KJ0001', 'HRD', 7000000, '2017-02-24 12:16:33', '2017-02-24 12:16:33'),
+(2, 'KJ0002', 'Bendahara', 6000000, '2017-02-24 12:17:57', '2017-02-24 12:17:57'),
+(3, 'KJ0003', 'Bagian IT', 6600000, '2017-02-24 12:18:52', '2017-02-24 12:18:52'),
+(4, 'KJ0004', 'Permesinan', 6000000, '2017-02-24 12:22:27', '2017-02-24 12:22:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_lemburs`
+--
+
+CREATE TABLE `kategori_lemburs` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `kode_lembur` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `jabatan_id` int(10) UNSIGNED NOT NULL,
+  `golongan_id` int(10) UNSIGNED NOT NULL,
+  `besaran_uang` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `kategori_lemburs`
+--
+
+INSERT INTO `kategori_lemburs` (`id`, `kode_lembur`, `jabatan_id`, `golongan_id`, `besaran_uang`, `created_at`, `updated_at`) VALUES
+(1, 'KL0001', 3, 1, 50000, '2017-02-24 12:37:14', '2017-02-24 12:37:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lembur_pegawais`
+--
+
+CREATE TABLE `lembur_pegawais` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `kode_lembur_id` int(10) UNSIGNED NOT NULL,
+  `pegawai_id` int(10) UNSIGNED NOT NULL,
+  `jmlh_jam` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `lembur_pegawais`
+--
+
+INSERT INTO `lembur_pegawais` (`id`, `kode_lembur_id`, `pegawai_id`, `jmlh_jam`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 3, '2017-02-24 12:37:36', '2017-02-24 12:37:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2017_01_30_084526_create_jabatans_table', 1),
+(4, '2017_01_30_090312_create_golongans_table', 1),
+(5, '2017_01_30_090818_create_kategori_lemburs_table', 1),
+(6, '2017_01_30_094518_create_tunjangans_table', 1),
+(7, '2017_01_30_100212_create_pegawais_table', 1),
+(8, '2017_01_30_100722_create_lembur_pegawais_table', 1),
+(9, '2017_01_30_101139_create_tunjangan_pegawais_table', 1),
+(10, '2017_01_30_101352_create_penggajians_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pegawais`
+--
+
+CREATE TABLE `pegawais` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nip` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `jabatan_id` int(10) UNSIGNED NOT NULL,
+  `golongan_id` int(10) UNSIGNED NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `pegawais`
+--
+
+INSERT INTO `pegawais` (`id`, `nip`, `user_id`, `jabatan_id`, `golongan_id`, `photo`, `created_at`, `updated_at`) VALUES
+(1, '160419991933', 3, 1, 1, '342225ba3f8a75d5039455fdd00d7142.jpg', '2017-02-24 12:28:11', '2017-02-24 12:31:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penggajians`
+--
+
+CREATE TABLE `penggajians` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `tunjangan_pegawai_id` int(10) UNSIGNED NOT NULL,
+  `jumlah_jam_lembur` int(11) NOT NULL,
+  `jumlah_uang_lembur` int(11) NOT NULL,
+  `gaji_pokok` int(11) NOT NULL,
+  `total_gaji` int(11) NOT NULL,
+  `tgl_pengambilan` date NOT NULL,
+  `status_pengambilan` tinyint(1) NOT NULL,
+  `petugas_penerima` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `penggajians`
+--
+
+INSERT INTO `penggajians` (`id`, `tunjangan_pegawai_id`, `jumlah_jam_lembur`, `jumlah_uang_lembur`, `gaji_pokok`, `total_gaji`, `tgl_pengambilan`, `status_pengambilan`, `petugas_penerima`, `created_at`, `updated_at`) VALUES
+(2, 1, 0, 0, 12000000, 12000000, '2024-02-17', 1, 'rikudo', '2017-02-24 12:46:37', '2017-02-24 12:46:37');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tunjangans`
+--
+
+CREATE TABLE `tunjangans` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `kode_tunjangan` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `jabatan_id` int(10) UNSIGNED NOT NULL,
+  `golongan_id` int(10) UNSIGNED NOT NULL,
+  `status` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `jumlah_anak` int(11) NOT NULL,
+  `besaran_uang` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `tunjangans`
+--
+
+INSERT INTO `tunjangans` (`id`, `kode_tunjangan`, `jabatan_id`, `golongan_id`, `status`, `jumlah_anak`, `besaran_uang`, `created_at`, `updated_at`) VALUES
+(1, 'KT0001', 3, 1, 'Belum menikah', 0, 1500000, '2017-02-24 12:32:46', '2017-02-24 12:32:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tunjangan_pegawais`
+--
+
+CREATE TABLE `tunjangan_pegawais` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `kode_tunjangan_id` int(10) UNSIGNED NOT NULL,
+  `pegawai_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `tunjangan_pegawais`
+--
+
+INSERT INTO `tunjangan_pegawais` (`id`, `kode_tunjangan_id`, `pegawai_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '2017-02-24 12:43:11', '2017-02-24 12:43:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `permission` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `permission`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'rizal', 'rizal@gmail.com', '$2y$10$iGQ2Iz5Xr963p8E.TqQsfOpF.hqci/ranW5e51rCOsghRN29Zx3q.', 'HRD', 'BuArWtKC48fr7p6UjZSRc0PDxPLvoQIRej1WrEz5QgQITOy8QW5Iohe7utNn', '2017-02-24 11:21:52', '2017-02-24 11:32:57'),
+(2, 'rikudo', 'rikudo@gmail.com', '$2y$10$VTyRz7kd2m2s6xXwhO.W6OroAElq/A.uXsb6IaVMcOaoMVn81NXl6', 'Admin', NULL, '2017-02-24 11:34:15', '2017-02-24 11:34:15'),
+(3, 'rizal', 'rizaln@gmail.com', '$2y$10$2QoE3Eglbz.I9gUMdprW.uPus30KqjfQHskCO.5xRsmU3UgwIZOSe', 'Pegawai', NULL, '2017-02-24 12:28:10', '2017-02-24 12:28:10');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `golongans`
+--
+ALTER TABLE `golongans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jabatans`
+--
+ALTER TABLE `jabatans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori_lemburs`
+--
+ALTER TABLE `kategori_lemburs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kategori_lemburs_jabatan_id_foreign` (`jabatan_id`),
+  ADD KEY `kategori_lemburs_golongan_id_foreign` (`golongan_id`);
+
+--
+-- Indexes for table `lembur_pegawais`
+--
+ALTER TABLE `lembur_pegawais`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `lembur_pegawais_kode_lembur_id_foreign` (`kode_lembur_id`),
+  ADD KEY `lembur_pegawais_pegawai_id_foreign` (`pegawai_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`),
+  ADD KEY `password_resets_token_index` (`token`);
+
+--
+-- Indexes for table `pegawais`
+--
+ALTER TABLE `pegawais`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `pegawais_nip_unique` (`nip`),
+  ADD UNIQUE KEY `pegawais_user_id_unique` (`user_id`),
+  ADD KEY `pegawais_jabatan_id_foreign` (`jabatan_id`),
+  ADD KEY `pegawais_golongan_id_foreign` (`golongan_id`);
+
+--
+-- Indexes for table `penggajians`
+--
+ALTER TABLE `penggajians`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `penggajians_tunjangan_pegawai_id_foreign` (`tunjangan_pegawai_id`);
+
+--
+-- Indexes for table `tunjangans`
+--
+ALTER TABLE `tunjangans`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tunjangans_jabatan_id_foreign` (`jabatan_id`),
+  ADD KEY `tunjangans_golongan_id_foreign` (`golongan_id`);
+
+--
+-- Indexes for table `tunjangan_pegawais`
+--
+ALTER TABLE `tunjangan_pegawais`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tunjangan_pegawais_kode_tunjangan_id_foreign` (`kode_tunjangan_id`),
+  ADD KEY `tunjangan_pegawais_pegawai_id_foreign` (`pegawai_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `golongans`
+--
+ALTER TABLE `golongans`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `jabatans`
+--
+ALTER TABLE `jabatans`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `kategori_lemburs`
+--
+ALTER TABLE `kategori_lemburs`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `lembur_pegawais`
+--
+ALTER TABLE `lembur_pegawais`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `pegawais`
+--
+ALTER TABLE `pegawais`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `penggajians`
+--
+ALTER TABLE `penggajians`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tunjangans`
+--
+ALTER TABLE `tunjangans`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tunjangan_pegawais`
+--
+ALTER TABLE `tunjangan_pegawais`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kategori_lemburs`
+--
+ALTER TABLE `kategori_lemburs`
+  ADD CONSTRAINT `kategori_lemburs_golongan_id_foreign` FOREIGN KEY (`golongan_id`) REFERENCES `golongans` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `kategori_lemburs_jabatan_id_foreign` FOREIGN KEY (`jabatan_id`) REFERENCES `jabatans` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `lembur_pegawais`
+--
+ALTER TABLE `lembur_pegawais`
+  ADD CONSTRAINT `lembur_pegawais_kode_lembur_id_foreign` FOREIGN KEY (`kode_lembur_id`) REFERENCES `kategori_lemburs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `lembur_pegawais_pegawai_id_foreign` FOREIGN KEY (`pegawai_id`) REFERENCES `pegawais` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pegawais`
+--
+ALTER TABLE `pegawais`
+  ADD CONSTRAINT `pegawais_golongan_id_foreign` FOREIGN KEY (`golongan_id`) REFERENCES `golongans` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pegawais_jabatan_id_foreign` FOREIGN KEY (`jabatan_id`) REFERENCES `jabatans` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pegawais_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `penggajians`
+--
+ALTER TABLE `penggajians`
+  ADD CONSTRAINT `penggajians_tunjangan_pegawai_id_foreign` FOREIGN KEY (`tunjangan_pegawai_id`) REFERENCES `tunjangan_pegawais` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tunjangans`
+--
+ALTER TABLE `tunjangans`
+  ADD CONSTRAINT `tunjangans_golongan_id_foreign` FOREIGN KEY (`golongan_id`) REFERENCES `golongans` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tunjangans_jabatan_id_foreign` FOREIGN KEY (`jabatan_id`) REFERENCES `jabatans` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tunjangan_pegawais`
+--
+ALTER TABLE `tunjangan_pegawais`
+  ADD CONSTRAINT `tunjangan_pegawais_kode_tunjangan_id_foreign` FOREIGN KEY (`kode_tunjangan_id`) REFERENCES `tunjangans` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tunjangan_pegawais_pegawai_id_foreign` FOREIGN KEY (`pegawai_id`) REFERENCES `pegawais` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
